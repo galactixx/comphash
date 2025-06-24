7e7ebd171f9075ee82d516a2770143816bbcfd99
galactixx <nwlezien.github@gmail.com>
2025-06-24 13:08:17 -0500

    register the module in build.zig

--- build.zig
+++ build.zig
@@ -1,39 +1,36 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    const root_source_file = "src/lib.zig";
+
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    // create the library module for the comptime hash map
-    const lib_mod = b.createModule(.{
-        .root_source_file = b.path("src/lib.zig"),
+    const ch_mod = b.addModule("comphash", .{
+        .root_source_file = b.path(root_source_file),
         .target = target,
         .optimize = optimize,
     });
 
-    const lib = b.addLibrary(.{
+    _ = b.addLibrary(.{
         .name = "comphash",
         .linkage = .static,
-        .root_module = lib_mod,
+        .root_module = ch_mod,
     });
-    lib.addIncludePath(b.path("include/"));
-    b.installArtifact(lib);
 
     // add the zighash dependency for the hash function
     const zh_pkg = b.dependency("zighash", .{ .target = target, .optimize = optimize });
     const zh_mod = zh_pkg.module("zighash");
-    lib_mod.addImport("zighash", zh_mod);
+    ch_mod.addImport("zighash", zh_mod);
 
-    const hash_tests = b.addTest(.{
-        .root_source_file = b.path("src/comphash.zig"),
-        .target = target,
-        .optimize = optimize,
+    // add the unit tests for the comphash library
+    const unit_tests = b.addTest(.{
+        .root_source_file = b.path(root_source_file),
     });
+    unit_tests.root_module.addImport("zighash", zh_mod);
 
-    // add the dependencies to the test module
-    hash_tests.root_module.addImport("zighash", zh_mod);
-
-    const run_tests = b.addRunArtifact(hash_tests);
+    // run the unit tests for the comphash library
+    const run_unit_tests = b.addRunArtifact(unit_tests);
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_tests.step);
+    test_step.dependOn(&run_unit_tests.step);
 }
